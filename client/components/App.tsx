972ba23a636921bdf07b1c7ae6f627bdd2f4b9cf
Humaid Memee <humaidmemee@gmail.com>
2023-09-06 10:00:02 +1200

    add use gesture react

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,37 +1,118 @@
-import React from 'react'
-import { useDrag } from 'react-dnd'
+import React, { useState } from 'react'
+// import { useSpring, animated } from '@react-spring/web'
+import { useDrag } from '@use-gesture/react'
 
-const ItemTypes = {
-  CARD: 'card', // Replace with the appropriate item type
-}
+function App() {
+  const [keyboard, setKeyboard] = useState({ x: 300, y: 500 })
+  const bindKeyboard = useDrag((params) => {
+    params.event.preventDefault()
+    setKeyboard({
+      x: params.offset[0],
+      y: params.offset[1],
+    })
+  })
 
-const App = () => {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: ItemTypes.CARD, // Use the appropriate type
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }))
+  const [mouse, setMouse] = useState({ x: 315, y: 535 })
+  const bindMouse = useDrag((params) => {
+    params.event.preventDefault()
+    setMouse({
+      x: params.offset[0],
+      y: params.offset[1],
+    })
+  })
+
+  const [pencils, setPencils] = useState({ x: 880, y: 425 })
+  const bindPencils = useDrag((params) => {
+    params.event.preventDefault()
+    setPencils({
+      x: params.offset[0],
+      y: params.offset[1],
+    })
+  })
 
   return (
-    <div className="container">
+    <div className="stationery">
       <div
-        className="test-div"
-        ref={drag}
-        style={{ opacity: isDragging ? 0.5 : 1 }}
+        {...bindKeyboard()}
+        style={{
+          position: 'relative',
+          width: '15rem',
+          height: 'auto',
+          top: keyboard.y,
+          left: keyboard.x,
+          zIndex: 2,
+        }}
       >
-        1
+        <div className="test-div"></div>
       </div>
+
       <div
-        className="test-div"
-        ref={drag}
-        style={{ opacity: isDragging ? 0.5 : 1 }}
+        {...bindMouse()}
+        style={{
+          position: 'relative',
+          width: '2rem',
+          height: 'auto',
+          top: mouse.y,
+          left: mouse.x,
+          zIndex: 2,
+        }}
       >
-        2
+        <div className="test-div"></div>
       </div>
-      <p>AAAAAA</p>
+
+      <div
+        {...bindPencils()}
+        style={{
+          position: 'relative',
+          width: '4rem',
+          height: 'auto',
+          top: pencils.y,
+          left: pencils.x,
+          zIndex: 3,
+        }}
+      >
+        <div className="test-div"></div>
+      </div>
     </div>
   )
 }
 
 export default App
+
+// import React from 'react'
+// import { useDrag } from 'react-dnd'
+
+// const ItemTypes = {
+//   CARD: 'card', // Replace with the appropriate item type
+// }
+
+// const App = () => {
+//   const [{ isDragging }, drag] = useDrag(() => ({
+//     type: ItemTypes.CARD, // Use the appropriate type
+//     collect: (monitor) => ({
+//       isDragging: !!monitor.isDragging(),
+//     }),
+//   }))
+
+//   return (
+//     <div className="container">
+//       <div
+//         className="test-div"
+//         ref={drag}
+//         style={{ opacity: isDragging ? 0.5 : 1 }}
+//       >
+//         1
+//       </div>
+//       <div
+//         className="test-div"
+//         ref={drag}
+//         style={{ opacity: isDragging ? 0.5 : 1 }}
+//       >
+//         2
+//       </div>
+//       <p>AAAAAA</p>
+//     </div>
+//   )
+// }
+
+// export default App

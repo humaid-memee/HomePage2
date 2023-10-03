33a57384717e610c319a899fb11d346e170f7d81
Humaid Memee <humaidmemee@gmail.com>
2023-10-04 05:48:11 +1300

    remove react dnd provider

--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { DndProvider } from 'react-dnd'
 
 import App from './components/App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
-  )
+  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
 })

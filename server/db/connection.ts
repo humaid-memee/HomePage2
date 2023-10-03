79f630b3796123ca7f44a106bc7f3aad7dbb9414
Humaid Memee <humaidmemee@gmail.com>
2023-10-04 05:27:16 +1300

    fix connection

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,8 +1,8 @@
-import config from './knexfile.js'
+import config from './knexfile'
 import knex from 'knex'
 
 type Environment = 'production' | 'test' | 'development'
 const environment = (process.env.NODE_ENV as Environment) || 'development'
-const connection = knex.default(config[environment])
+const connection = knex(config[environment])
 
 export default connection

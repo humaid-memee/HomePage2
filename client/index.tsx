77bb8ef91484ec9304eeadf49cd06e990a076ee2
Humaid Memee <humaidmemee@gmail.com>
2023-10-05 17:45:40 +1300

    set up query client

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,14 @@
 import { createRoot } from 'react-dom/client'
 
 import App from './components/App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  )
 })
